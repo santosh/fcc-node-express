7c15a310b4b05e9b2afee8a81f9a59754179a06b
Santosh Kumar <sntshkmr60@gmail.com>
2022-04-14 00:51:05 +0000

    Send JSON

--- myApp.js
+++ myApp.js
@@ -4,20 +4,17 @@ var app = express();
 
 let staticPath = __dirname + "/public"
 
+
 app.use("/public", express.static(staticPath))
-
-
-
-
-
-
 
 app.get("/", (req, res) => {
   formTemplate = __dirname + "/views/index.html"
   res.sendFile(formTemplate)
 })
 
-
+app.get("/json", (req, res) => {
+  res.json({ "message": "Hello json" })
+})
 
 
 
